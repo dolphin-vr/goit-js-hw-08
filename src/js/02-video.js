20cfdaa7e25ab1688dd90aeb715859427514ffe5
dolphin-vr <fun53@ukr.net>
2023-07-04 01:10:27 +0300

    t2a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,41 +1,19 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
 const timer = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', timing);
+const position = JSON.parse(localStorage.getItem(timer)) || {seconds: 0};
+console.log('position = ', position);
+console.log('position.seconds = ', position.seconds);
 
-// player.getVideoTitle().then(onTime(title) {
-//    console.log('title:', title);
-// });
+player.setCurrentTime(position.seconds);
+
+
+player.on('timeupdate', throttle(timing, 1000));
 
 function timing(data){
    localStorage.setItem(timer, JSON.stringify(data))
 }
-
-// timeupdate
-// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
-// {
-//     duration: 61.857
-//     percent: 0.049
-//     seconds: 3.034
-// }
-
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// const onPlay = function(data) {
-//    // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-
-
-
