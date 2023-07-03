7d079a5a5d4d0c66dfe986b6511813d9ae7c4af9
dolphin-vr <fun53@ukr.net>
2023-07-03 21:18:49 +0300

    t2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const ulRef = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({preview, original, description})=>
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join('');
+ulRef.innerHTML = galleryMarkup;
 
-console.log(galleryItems);
+const opts = {
+   captionsData: 'alt',
+   captionDelay: 250,
+};
+const lightbox = new SimpleLightbox('.gallery a',opts);
+
+// console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
+
+const timer = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', timing);
+
+// player.getVideoTitle().then(onTime(title) {
+//    console.log('title:', title);
+// });
+
+function timing(data){
+   localStorage.setItem(timer, JSON.stringify(data))
+}
+
+// timeupdate
+// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// const onPlay = function(data) {
+//    // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+
 
