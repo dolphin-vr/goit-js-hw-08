3db450ff1045ed4dcfb58b54c3ce80e05405e621
dolphin-vr <fun53@ukr.net>
2023-07-04 02:59:22 +0300

    t3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const key = "feedback-form-state";
+const feedback = JSON.parse(localStorage.getItem(key)) || {email: '', message: ''};
+form.email.value = feedback.email;
+form.message.value = feedback.message;
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput (){
+   feedback.email = form.email.value;
+   feedback.message = form.message.value;
+   localStorage.setItem(key, JSON.stringify(feedback))
+}
+
+function onSubmit(ev){
+   ev.preventDefault();
+   console.dir(feedback);
+   localStorage.removeItem(key);
+   form.email.value = '';
+   form.message.value = '';
+}
